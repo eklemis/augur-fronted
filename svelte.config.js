1d7df6bff1329ceeb6cd93f8d820cef91bf48304
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2023-11-14 23:10:28 +0700

    learn github actions-add steps to build and start production app

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			envPrefix: '',
+			polyfill: true
+		})
 	},
 	preprocess: vitePreprocess()
 };
